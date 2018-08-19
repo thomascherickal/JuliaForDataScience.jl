826def1bd39bacbbd706f4c33b9363d6d0d3cc0e
Josh Day <emailjoshday@gmail.com>
2018-08-19 09:52:03 -0400

    updates

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,17 @@
 using Documenter, JuliaForDataScience
 
-makedocs()
+makedocs(
+    format = :html,
+    modules = [JuliaForDataScience],
+    sitename = "Julia For Data Science",
+    pages = [
+        "intro.md"
+    ]
+)
+
+deploydocs(
+    repo   = "github.com/USER/PKG.jl.git",
+    target = "build",
+    deps   = nothing,
+    make   = nothing
+)
