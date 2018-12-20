1febd66fdc5e6fe27a10802bbea67fc9f80ff6ab
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2018-12-20 19:52:14 +0000

    build based on 120f244

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,24 +1,64 @@
 var documenterSearchIndex = {"docs": [
 
 {
-    "location": "01-whyjulia/#",
-    "page": "-",
-    "title": "-",
-    "category": "page",
-    "text": "(Image: )If you are interested in how and why Julia exists at all, a good read by its creators is here: Why We Created Julia.  Below is  an excerpt:We love [Matlab/Lisp/Python/Ruby/Perl/Mathematica/R/C]; they are wonderful and powerful. For the work we do — scientific computing, machine learning, data mining, large-scale linear algebra, distributed and parallel computing — each one is perfect for some aspects of the work and terrible for others. Each one is a trade-off.  We are greedy: we want more."
-},
-
-{
-    "location": "02-juliabasics/#",
-    "page": "The REPL",
-    "title": "The REPL",
+    "location": "00-index/#",
+    "page": "Julia For Data Science",
+    "title": "Julia For Data Science",
     "category": "page",
     "text": ""
 },
 
 {
+    "location": "00-index/#Julia-For-Data-Science-1",
+    "page": "Julia For Data Science",
+    "title": "Julia For Data Science",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "00-index/#Table-of-Contents-1",
+    "page": "Julia For Data Science",
+    "title": "Table of Contents",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "01-whyjulia/#",
+    "page": "Why Julia?",
+    "title": "Why Julia?",
+    "category": "page",
+    "text": "(Image: )"
+},
+
+{
+    "location": "01-whyjulia/#Why-Julia?-1",
+    "page": "Why Julia?",
+    "title": "Why Julia?",
+    "category": "section",
+    "text": "If you are interested in how and why Julia exists at all, a good read by its creators is here: Why We Created Julia.  Below is  an excerpt:We love [Matlab/Lisp/Python/Ruby/Perl/Mathematica/R/C]; they are wonderful and powerful. For the work we do — scientific computing, machine learning, data mining, large-scale linear algebra, distributed and parallel computing — each one is perfect for some aspects of the work and terrible for others. Each one is a trade-off.  We are greedy: we want more."
+},
+
+{
+    "location": "02-juliabasics/#",
+    "page": "Julia Basics",
+    "title": "Julia Basics",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "02-juliabasics/#Julia-Basics-1",
+    "page": "Julia Basics",
+    "title": "Julia Basics",
+    "category": "section",
+    "text": ""
+},
+
+{
     "location": "02-juliabasics/#The-REPL-1",
-    "page": "The REPL",
+    "page": "Julia Basics",
     "title": "The REPL",
     "category": "section",
     "text": "Your first usage of Julia will probably be with the REPL (read-eval-print-loop).x = 1\n\ny = 2.0"
@@ -26,7 +66,7 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "02-juliabasics/#Types-1",
-    "page": "The REPL",
+    "page": "Julia Basics",
     "title": "Types",
     "category": "section",
     "text": ""
@@ -34,7 +74,7 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "02-juliabasics/#Multiple-Dispatch-1",
-    "page": "The REPL",
+    "page": "Julia Basics",
     "title": "Multiple Dispatch",
     "category": "section",
     "text": ""
@@ -42,7 +82,7 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "02-juliabasics/#Package-Management-1",
-    "page": "The REPL",
+    "page": "Julia Basics",
     "title": "Package Management",
     "category": "section",
     "text": ""
@@ -50,42 +90,26 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "03-visualization/#",
-    "page": "Plots.jl",
-    "title": "Plots.jl",
+    "page": "Data Visualization",
+    "title": "Data Visualization",
     "category": "page",
     "text": "(Image: )ENV[\"GKSwstype\"] = \"100\""
 },
 
 {
+    "location": "03-visualization/#Data-Visualization-1",
+    "page": "Data Visualization",
+    "title": "Data Visualization",
+    "category": "section",
+    "text": ""
+},
+
+{
     "location": "03-visualization/#Plots.jl-1",
-    "page": "Plots.jl",
+    "page": "Data Visualization",
     "title": "Plots.jl",
     "category": "section",
-    "text": "There are a variety of data viz tools in Julia, but this chapter will focus on the Plots package, which provides an interface for writing plot commands that work with multiple \"backends\", including GR and plotlyusing Plots\n\nscatter(rand(10))\npng(\"plot1.png\"); nothing # hide(Image: )"
-},
-
-{
-    "location": "#",
-    "page": "Julia For Data Science",
-    "title": "Julia For Data Science",
-    "category": "page",
-    "text": ""
-},
-
-{
-    "location": "#Julia-For-Data-Science-1",
-    "page": "Julia For Data Science",
-    "title": "Julia For Data Science",
-    "category": "section",
-    "text": ""
-},
-
-{
-    "location": "#Table-of-Contents-1",
-    "page": "Julia For Data Science",
-    "title": "Table of Contents",
-    "category": "section",
-    "text": ""
+    "text": "There are a variety of data viz tools in Julia, but this chapter will focus on the Plots package, which provides an interface for writing plot commands that work with multiple \"backends\", including GR and plotlyusing Plots\n\nscatter(rand(10))\n\npng(\"plot1.png\"); nothing # hide(Image: )"
 },
 
 {
